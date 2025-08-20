9aaaa5d05b614220193e4c53f047993ecce42954
Sachdeva12 <kasihsh825@gmail.com>
2025-08-20 23:43:13 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,3 @@
-export default function Home() {
-  // Only for testing - do not use in production
-  const apiKeyPreview = process.env.OPENAI_API_KEY
-    ? process.env.NEXT_PUBLIC_OPENAI_KEY.slice(0, 5) + "*****"
-    : "❌ Not Loaded";
-
-  return (
-    <div style={{ fontFamily: "sans-serif", padding: "2rem" }}>
-      <h1>API is Running 🚀</h1>
-      <p>Key Status: {apiKeyPreview}</p>
-    </div>
-  );
-}
+update this export default function Home() {
+  return <h1>API is Running 🚀</h1>
+}'
