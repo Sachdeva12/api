be082e45b4b322a837fe609eb4acbef2c62f118b
Sachdeva12 <kasihsh825@gmail.com>
2025-09-13 15:06:18 +0530

    Update generate-review.js

--- pages/api/generate-review.js
+++ pages/api/generate-review.js
@@ -1,5 +1,4 @@
-
-export default async function handler(req, res) {
+ export default async function handler(req, res) {
   // ✅ Allow CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
